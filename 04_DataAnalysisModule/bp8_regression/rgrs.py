4d652457f42004a78434f7b9a98c6cb0ae17f3b0
haneul yeon <yun901130@naver.com>
2021-01-26 18:17:53 +0900

    회귀

--- 04_DataAnalysisModule/bp8_regression/rgrs.py
+++ 04_DataAnalysisModule/bp8_regression/rgrs.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 from my_util.weather import get_weather
+import matplotlib.pyplot as plt
 
 rgrs_bp = Blueprint('rgrs_bp', __name__)
 
@@ -26,7 +27,7 @@ def get_weather_main():
 
 @rgrs_bp.route('/iris', methods=['GET', 'POST'])
 def iris():
-    menu = {'ho':0, 'da':0, 'ml':10, 
+    menu = {'ho':0, 'da':0, 'ml':1, 
             'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
             'cf':0, 'ac':0, 're':1, 'cu':0}
     if request.method == 'GET':
@@ -60,6 +61,78 @@ def iris():
         pred[feature_name] = np.round(pred_value, 2)
         return render_template('regression/iris_res.html', menu=menu, weather=get_weather(),
                                 index=index, org=org, pred=pred, feature=column_dict[feature_name])
+
+@rgrs_bp.route('/diabets', methods=['GET', 'POST'])
+def diabets():
+    menu = {'ho':0, 'da':0, 'ml':1, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
+            'cf':0, 'ac':0, 're':1, 'cu':0}
+    if request.method == 'GET':
+        return render_template('regression/diabets.html', menu=menu, weather=get_weather())
+    else:
+        plt.figure()
+        index = int(request.form['index'])
+        feature = request.form['feature']
+        df = pd.read_csv('static/data/diabetes_train.csv')
+        X = df[feature].values.reshape(-1,1)
+        y = df.target.values
+
+        lr = LinearRegression()
+        lr.fit(X, y)
+        weight, bias = lr.coef_, lr.intercept_
+        
+        df_test = pd.read_csv('static/data/diabetes_test.csv')
+        X_test = df_test[feature][index]
+        y_test = df_test.target[index]
+        pred = X_test * weight[0] + bias
+
+        y_min = np.min(X) * weight[0] + bias
+        y_max = np.max(X) * weight[0] + bias
+        plt.scatter(X, y, label='train')
+        plt.plot([np.min(X), np.max(X)], [y_min, y_max], 'r', lw=3)
+        plt.scatter([X_test], [y_test], c='r', marker='*', s=100, label='test')
+        plt.grid()
+        plt.legend()
+        plt.title(f'Diabets target vs {feature}')
+        img_file = os.path.join(current_app.root_path, 'static/img/diabets.png')
+        plt.savefig(img_file)
+        mtime = int(os.stat(img_file).st_mtime)
+
+        result_dict = {'index':index, 'feature':feature, 'y':y_test, 'pred':pred}
+        return render_template('regression/diabets_res.html', menu=menu, weather=get_weather(),
+                                res=result_dict, mtime=mtime)
+
+@rgrs_bp.route('/boston', methods=['GET', 'POST'])
+def boston():
+    menu = {'ho':0, 'da':0, 'ml':1, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
+            'cf':0, 'ac':0, 're':1, 'cu':0}
+    if request.method == 'GET':
+        feature_list = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
+                        'TAX', 'PTRATIO', 'B', 'LSTAT']
+        return render_template('regression/boston.html', feature_list=feature_list,
+                               menu=menu, weather=get_weather())
+    else:
+        index = int(request.form['index'])
+        feature_list = request.form.getlist('feature')
+        df = pd.read_csv('static/data/boston_train.csv')
+        X = df[feature_list].values
+        y = df.target.values
+
+        lr = LinearRegression()
+        lr.fit(X, y)
+        weight, bias = lr.coef_, lr.intercept_
+
+        df_test = pd.read_csv('static/data/boston_test.csv')
+        X_test = df_test[feature_list].values[index, :]
+        y_test = df_test.target[index]
+        pr = lr.predict(X_test.reshape(1,-1))
+        pred = np.round(pr[0])
+
+        result_dict = {'index':index, 'feature':feature_list, 'y':y_test, 'pred':pred}
+        org = dict(zip(df.columns[:-1], df_test.iloc[index, :-1]))
+        return render_template('regression/boston_res.html', res=result_dict, org=org,
+                               menu=menu, weather=get_weather())
 
 kospi_dict, kosdaq_dict, nyse_dict, nasdaq_dict = {}, {}, {}, {}
 @rgrs_bp.before_app_first_request
